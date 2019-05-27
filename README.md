657d03ded649ea260b22327b669e6e6097ccfe9a
sanekua <sasha-kuzmyk@mail.ru>
2019-05-27 04:18:17 +0300

    assistant.py

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # phone_assistant
+This is python Phone assistant programs with pytest
+# Running the Project Locally
+
+## Installation
+First, clone the repository to your local machine:
+```
+git clone https://github.com/sanekua/phone_assistant.git
+
+```
+Install the requirements:
+```
+pip install -r requirements.txt
+```
+
+```
+You can connect your sqlite3 as test.db:
+```
+
+You can execute the program with following command :
+```
+python assistant.py
+```
--- assistant.py
+++ assistant.py
@@ -1,19 +1,55 @@
 import re
-numbers = ['06$%^&*(72456845', '0672178965', '0982255468','067245888','06756845',
-           '067856845','0982456845','09832456845','0972456845','0992456845','099092456845','0902456845'
-           ]
+import sqlite3 as lite
+
+
+con = lite.connect('test.db')
+def create_database():
+    with con:
+        cur = con.cursor()
+        cur.execute("CREATE TABLE Phone(Name TEXT)")
+        cur.execute("INSERT INTO Phone VALUES('06$%^&*(72456845')")
+        cur.execute("INSERT INTO Phone VALUES('0672178965')")
+        cur.execute("INSERT INTO Phone VALUES(0982255468)")
+        cur.execute("INSERT INTO Phone VALUES(067245888)")
+        cur.execute("INSERT INTO Phone VALUES(06756845)")
+        cur.execute("INSERT INTO Phone VALUES(067856845)")
+        cur.execute("INSERT INTO Phone VALUES(0982456845)")
+        cur.execute("INSERT INTO Phone VALUES(09832456845)")
+        cur.execute("INSERT INTO Phone VALUES(0972456845)")
+        cur.execute("INSERT INTO Phone VALUES(0992456845)")
+        cur.execute("INSERT INTO Phone VALUES(099092456845)")
+        cur.execute("INSERT INTO Phone VALUES(0902456845)")
 
 def program(numbers):
     l = []
     k = 0
-    n = input('input something')
+    n = input('Please, input beggining of the phone number : ')
     for i in numbers:
         if i.startswith(n) and re.match(r'((8|\+3)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}',i):
             l.append(i)
             k += 1
             if k == 10:
                 break
-    return l
+    if len(l) == 0:
+        return 'No such number in our database'
+    else:
+        return l
 
 
-print(program(numbers))
+
+def main_part():
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM Phone")
+        l = []
+        while True:
+            row = cur.fetchone()
+            if row == None:
+                break
+            l.append(row[0])
+        print(program(l))
+
+
+create_database()
+main_part()
+
